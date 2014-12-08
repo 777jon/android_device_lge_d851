ad41720359696f5c1b0bad229c80c133728a7fa6
777jon <jcruzdeveloper@gmail.com>
2014-12-08 01:49:07 -0500

    d851: TWRP bringup

--- omni_d851.mk
+++ omni_d851.mk
@@ -1,5 +1,5 @@
 #
-# Copyright 2014 The CyanogenMod Project
+# Copyright 2014 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,15 +14,21 @@
 # limitations under the License.
 #
 
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+# Get the prebuilt list of APNs
+$(call inherit-product, vendor/omni/config/gsm.mk)
 
-# Inherit from d851 device
-$(call inherit-product, device/lge/d851/device.mk)
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+
+# Inherit from our custom product configuration
+$(call inherit-product, vendor/omni/config/common.mk)
+
+PRODUCT_COPY_FILES += device/lge/d851/fstab.g3:recovery/root/fstab.g3
+PRODUCT_COPY_FILES += device/lge/d851/recovery.fstab:recovery/root/etc/twrp.fstab
 
 # Set those variables here to overwrite the inherited values.
 PRODUCT_DEVICE := d851
-PRODUCT_NAME := full_d851
-PRODUCT_BRAND := lge
+PRODUCT_NAME := omni_d851
+PRODUCT_BRAND := LG
 PRODUCT_MODEL := LG-D851
-PRODUCT_MANUFACTURER := LGE
+PRODUCT_MANUFACTURER := LG
